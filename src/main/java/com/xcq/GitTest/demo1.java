072c7f2f1c7fd238c81de50f27955e17fc44afd2
xuchangqing <1214043859@qq.com>
2022-11-22 23:15:28 +0800

    second commit

--- src/main/java/com/xcq/GitTest/demo1.java
+++ src/main/java/com/xcq/GitTest/demo1.java
@@ -3,9 +3,6 @@ package com.xcq.GitTest;
 public class demo1 {
     public static void main(String[] args) {
         System.out.println(1);
-        System.out.println(2);
-        System.out.println(2);
-        System.out.println(2);
-        System.out.println(2);
+
     }
 }
