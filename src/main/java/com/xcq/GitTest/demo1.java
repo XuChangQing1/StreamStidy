b8f05283dcdfe72dd4bb517ce442e1dee3c4f182
xuchangqing <1214043859@qq.com>
2022-11-22 23:27:29 +0800

    Revert "o4-commit"
    This reverts commit a1e8d7e78f097e87aeec3d9e191f054a07f8e73c.

--- src/main/java/com/xcq/GitTest/demo1.java
+++ src/main/java/com/xcq/GitTest/demo1.java
@@ -10,10 +10,5 @@ public class demo1 {
         System.out.println(2);
         System.out.println(2);
         System.out.println(3);
-        System.out.println(3);
-        System.out.println(3);
-        System.out.println(3);
-        System.out.println(3);
-
     }
 }
