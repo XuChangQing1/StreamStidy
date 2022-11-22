08110f8238525c404fbed15e19ebbeaf1aca8872
xuchangqing <1214043859@qq.com>
2022-11-22 23:22:01 +0800

    o4-commit

--- src/main/java/com/xcq/GitTest/demo1.java
+++ src/main/java/com/xcq/GitTest/demo1.java
@@ -3,6 +3,12 @@ package com.xcq.GitTest;
 public class demo1 {
     public static void main(String[] args) {
         System.out.println(1);
-
+        System.out.println(2);
+        System.out.println(2);
+        System.out.println(2);
+        System.out.println(2);
+        System.out.println(2);
+        System.out.println(2);
+        System.out.println(3);
     }
 }
