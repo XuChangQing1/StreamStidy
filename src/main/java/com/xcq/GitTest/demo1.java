02e2f4eb549ae0496042bb47b4abf57a0683b92b
XuChangQing1 <100475730+XuChangQing1@users.noreply.github.com>
2022-11-23 23:25:29 +0800

    Update demo1.java

--- src/main/java/com/xcq/GitTest/demo1.java
+++ src/main/java/com/xcq/GitTest/demo1.java
@@ -11,5 +11,9 @@ public class demo1 {
         System.out.println(2);
         System.out.println(3);
         System.out.println("wewewewew");
+        System.out.println("wewewewew");
+        System.out.println("wewewewew");
+        System.out.println("wewewewew");
+        System.out.println("wewewewew");
     }
 }
